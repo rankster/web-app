92241b3a6b0020c29a23d4c367c78e1a0c1965a5
vpoturaev <viacheslav.poturaev@lazada.com>
2016-07-31 01:48:55 +0700

    match details

--- src/View/UserPlate.php
+++ src/View/UserPlate.php
@@ -8,24 +8,26 @@ use Yaoi\View\Hardcoded;
 
 class UserPlate extends Hardcoded
 {
-    public function __construct(User $user)
+    public function __construct(User $user, $rank = null)
     {
         $this->user = $user;
+        $this->rank = $rank;
     }
 
     public $user;
+    public $rank;
 
     public function render()
     {
         $user = $this->user;
         echo <<<HTML
-  <div class="col-sm-6 col-lg-3" style="max-height:123px;">
-    <div class="card-box widget-user" style="min-height:123px;">
+  <div class="col-sm-6 col-lg-3">
+    <div class="card-box widget-user">
       <div>
         <img src="http://rankster.penix.tk/user-images{$user->picturePath}" class="img-responsive img-circle" alt="user">
         <div class="wid-u-info">
           <h4 class="m-t-0 m-b-5">{$user->name}</h4>
-          
+          <p class="text-muted m-b-5 font-13">{$this->rank}</p>          
           <small class="text-success"><b>Rookie</b></small>
         </div>
       </div>
