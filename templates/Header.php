c76cecae93b008acef6e8c92e275d19ee73d9158
Medhi Bechina <medhi.bechina@lazada.com>
2016-07-30 20:23:21 +0700

    Fix user description box

--- templates/Header.php
+++ templates/Header.php
@@ -1,53 +1,17 @@
+<?php if isset($_SESSION['user_name']): ?>
 <div class="row">
-                    <div class="col-sm-6 col-lg-3">
-                        <div class="card-box widget-user">
-                            <div>
-                            <img src="<?php echo $_SESSION['user_picture']; ?>" class="img-responsive img-circle" alt="user">
-                                <div class="wid-u-info">
-                                    <h4 class="m-t-0 m-b-5">Chadengle</h4>
-                                    <p class="text-muted m-b-5 font-13"><?php //echo $_SESSION['user_name']; ?></p>
-                                    <small class="text-warning"><b>Admin</b></small>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  <div class="col-sm-6 col-lg-3">
+    <div class="card-box widget-user">
+      <div>
+        <img src="<?php echo $_SESSION['user_picture']; ?>" class="img-responsive img-circle" alt="user">
+        <div class="wid-u-info">
+          <h4 class="m-t-0 m-b-5"><?php echo $_SESSION['user_name']; ?></h4>
+          <p class="text-muted m-b-5 font-13"><?php //echo $_SESSION['user_name']; ?></p>
+          <small class="text-warning"><b>Gamer - Rookie</b></small>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
-                    <div class="col-sm-6 col-lg-3">
-                        <div class="card-box widget-user">
-                            <div>
-                                <img src="assets/images/users/avatar-2.jpg" class="img-responsive img-circle" alt="user">
-                                <div class="wid-u-info">
-                                    <h4 class="m-t-0 m-b-5">Tomaslau</h4>
-                                    <p class="text-muted m-b-5 font-13">coderthemes@gmail.com</p>
-                                    <small class="text-success"><b>User</b></small>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-sm-6 col-lg-3">
-                        <div class="card-box widget-user">
-                            <div>
-                                <img src="assets/images/users/avatar-7.jpg" class="img-responsive img-circle" alt="user">
-                                <div class="wid-u-info">
-                                    <h4 class="m-t-0 m-b-5">Ok</h4>
-                                    <p class="text-muted m-b-5 font-13">coderthemes@gmail.com</p>
-                                    <small class="text-pink"><b>Admin</b></small>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-sm-6 col-lg-3">
-                        <div class="card-box widget-user">
-                            <div>
-                                <img src="assets/images/users/avatar-8.jpg" class="img-responsive img-circle" alt="user">
-                                <div class="wid-u-info">
-                                    <h4 class="m-t-0 m-b-5">Arashasghari</h4>
-                                    <p class="text-muted m-b-5 font-13">coderthemes@gmail.com</p>
-                                    <small class="text-info"><b>User</b></small>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+<?php endif; ?>
