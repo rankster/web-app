1ac3d2e600ba682cf3fed2c9860a21fa0e14d971
Medhi Bechina <medhi.bechina@lazada.com>
2016-07-30 21:43:18 +0700

    little swag on disconnect button

--- templates/Header.php
+++ templates/Header.php
@@ -1,6 +1,6 @@
 <?php if (isset($_SESSION['user_name'])): ?>
 <div class="row">
-  <div class="col-sm-6 col-lg-3" style="max-height:122px;">
+  <div class="col-sm-6 col-lg-3" style="min-height:123px;max-height:123px;">
     <div class="card-box widget-user">
       <div>
         <img src="http://<?php echo $_SESSION['user_picture']; ?>" class="img-responsive img-circle" alt="user">
@@ -8,7 +8,7 @@
           <h4 class="m-t-0 m-b-5"><?php echo $_SESSION['user_name']; ?></h4>
           <p class="text-muted m-b-5 font-13"><?php echo $_SESSION['user_email']; ?></p>
           <small class="text-success"><b>Rookie</b></small>
-          <a href="/logout" class="btn btn-default btn-xs" style="float:right">Logout</a>
+          <a href="/logout" class="btn btn-default btn-xs" style="float:right;margin-top:15px">Logout</a>
         </div>
       </div>
     </div>
