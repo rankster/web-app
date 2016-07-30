d4d6977131844034a4f0fc0b11c3eeff94dd4a1d
okapustin <oleksandr.kapustin@lazada.com>
2016-07-30 16:14:37 +0700

    [facebook login] fixed configs

--- env/conf.dist.php
+++ env/conf.dist.php
@@ -3,12 +3,12 @@
 namespace Rankster;
 
 use Rankster\Service\Facebook;
-use Rankster\Service\FacebookSettings;
+use Rankster\Service\Facebook\Settings;
 use Yaoi\Database;
 
 Database::register('mysqli://root:password@localhost/rankster');
 Facebook::register(function(){
-    $settings = new FacebookSettings();
+    $settings = new Settings();
     $settings->appId = 'app-id';
     $settings->appSecret = 'app-secret';
     return $settings;
