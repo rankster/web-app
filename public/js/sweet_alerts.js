7b278c75622bd742334bbed0f9ae0ed55e804c4c
okapustin <oleksandr.kapustin@lazada.com>
2016-07-31 05:13:48 +0700

    ajax form

--- public/js/sweet_alerts.js
+++ public/js/sweet_alerts.js
@@ -188,10 +188,9 @@ function sendCreateMatchForm() {
 
         var url = "/match/create"; // the script where you handle the form input.
 
-        $.post(url,
-            $("#create-match").serialize()
-        );
+        $.post(url, $("#create-match").serialize());
 
+        return false;
     });
 
     switch($( "input:radio[name=result]:checked" ).val()) {
