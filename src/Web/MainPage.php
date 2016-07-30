3a272ea59aea6d631be4b4be965e2f942bbe36b5
vpoturaev <viacheslav.poturaev@lazada.com>
2016-07-30 18:09:56 +0700

    sample

--- src/Web/MainPage.php
+++ src/Web/MainPage.php
@@ -8,6 +8,8 @@ use Yaoi\Command\Definition;
 
 class MainPage extends Command
 {
+    public $bitch = "Hello!";
+
     static function setUpDefinition(Definition $definition, $options)
     {
     }
@@ -16,6 +18,12 @@ class MainPage extends Command
     {
       $this->response->success("Hello! Test From Alexis");
       $this->response->addContent(file_get_contents("./Static/MainPage_tables.html"));
+
+        $ss = <<<HTML
+<p>Yeah, {$this->bitch}</p>
+HTML;
+        $this->response->addContent($ss);
+
     }
 
 
