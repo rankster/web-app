f5f27c9954816ef2041832ce829bb1b3bd233def
okapustin <oleksandr.kapustin@lazada.com>
2016-07-30 14:35:50 +0700

    [facebook login] config

--- env/conf.dist.php
+++ env/conf.dist.php
@@ -2,6 +2,12 @@
 
 namespace Rankster;
 
+use Rankster\Service\FacebookLogin;
 use Yaoi\Database;
 
 Database::register('mysqli://root:password@localhost/rankster');
+
+FacebookLogin::register('', 'app_id');
+FacebookLogin::register('', 'app_secret');
+FacebookLogin::register('2.2', 'default_graph_version');
+FacebookLogin::register('http://rankster.penix.tk/v1/login', 'callback_url');
---
+++ src/Service/FacebookLogin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Rankster\Service;
+
+use Yaoi\Service;
+
+class FacebookLogin extends Service
+{
+    public function callback()
+    {
+        
+    }
+}
