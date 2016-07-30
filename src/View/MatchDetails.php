749de2f9d5158f8b6460e68d573e4ac606478871
vpoturaev <viacheslav.poturaev@lazada.com>
2016-07-31 02:04:28 +0700

    match details

--- src/View/MatchDetails.php
+++ src/View/MatchDetails.php
@@ -27,8 +27,22 @@ class MatchDetails extends Hardcoded
         $rank2 = Rank::findOrCreateByUserGame($user2->id, $match->gameId);
 
         $gamePlate = (string)GamePlate::create(Game::findByPrimaryKey($match->gameId));
-        $user1Plate = (string)UserPlate::create($user1, $rank1->rank);
-        $user2Plate = (string)UserPlate::create($user2, $rank2->rank);
+        $user1Info = $rank1->rank;
+        $user2Info = $rank2->rank;
+
+        if ($match->winnerId === null) {
+            $user1Info .= ' draw';
+            $user2Info .= ' draw';
+        } elseif ($match->winnerId === $user1->id) {
+            $user1Info .= ' win';
+            $user2Info .= ' lose';
+        } else {
+            $user1Info .= ' lose';
+            $user2Info .= ' win';
+        }
+
+        $user1Plate = (string)UserPlate::create($user1, $user1Info);
+        $user2Plate = (string)UserPlate::create($user2, $user2Info);
 
         echo <<<HTML
 <div class="row">
