764096a899c221c2f6054963b51a93f5302fc754
vpoturaev <viacheslav.poturaev@lazada.com>
2016-07-30 23:07:52 +0700

    fb share

--- src/Twbs/Layout.php
+++ src/Twbs/Layout.php
@@ -81,6 +81,23 @@ class Layout extends Hardcoded
   <div class="container">
       <h1><a href="/"><img src="http://i.imgur.com/mQtR9aL.png" width="150" /></a></h1>
       <?php echo $this->main ?>
+
+      <!-- Load Facebook SDK for JavaScript -->
+      <div id="fb-root"></div>
+      <script>(function (d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s);
+              js.id = id;
+              js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1";
+              fjs.parentNode.insertBefore(js, fjs);
+          }(document, 'script', 'facebook-jssdk'));</script>
+
+      <!-- Your share button code -->
+      <div class="fb-share-button"
+           data-href="http://www.your-domain.com/your-page.html"
+           data-layout="button_count">
+      </div>
   </div>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
