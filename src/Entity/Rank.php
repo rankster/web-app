babc3397494ade98a32539d3f80498bb88f2ba93
vpoturaev <viacheslav.poturaev@lazada.com>
2016-07-30 22:37:32 +0700

    elo calc

--- src/Entity/Rank.php
+++ src/Entity/Rank.php
@@ -2,6 +2,7 @@
 
 namespace Rankster\Entity;
 
+use Alcalyn\Elo\EloSystem;
 use Yaoi\Database\Definition\Column;
 use Yaoi\Database\Definition\Table;
 use Yaoi\Database\Entity;
@@ -29,5 +30,24 @@ class Rank extends Entity
     static function setUpTable(\Yaoi\Database\Definition\Table $table, $columns)
     {
         $table->setSchemaName('rank');
+    }
+
+    public function draw(Rank $opponent)
+    {
+        $eloSystem = new EloSystem();
+
+        $updatedElos = $eloSystem->draw($this->rank, $opponent->rank);
+        $this->rank = $updatedElos[0];
+        $opponent->rank = $updatedElos[1];
+    }
+
+    public function win(Rank $loser)
+    {
+        $eloSystem = new EloSystem();
+
+        $updatedElos = $eloSystem->calculate($this->rank, $loser->rank, 1);
+
+        $this->rank = $updatedElos[0];
+        $loser->rank = $updatedElos[1];
     }
 }
