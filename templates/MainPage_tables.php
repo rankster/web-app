d38b5bb182d2b6a14ba6f2bf7c58509530743f6f
okapustin <oleksandr.kapustin@lazada.com>
2016-07-31 01:44:58 +0700

    [rank list] star

--- templates/MainPage_tables.php
+++ templates/MainPage_tables.php
@@ -15,7 +15,13 @@
                                 <tbody>
                                     <?php foreach (\Rankster\Entity\Rank::getRanks($game->id) as $i => $rank) : ?>
                                     <tr>
-                                        <th scope="row"><?php echo ($i + 1); ?></th>
+                                        <th scope="row">
+                                            <?php if ($i == 0): ?>
+                                                <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                                            <?php else: ?>
+                                            <?php echo ($i + 1); ?>
+                                            <?php endif; ?>
+                                        </th>
                                         <td><img class="img-circle" src="<?php echo \Rankster\Entity\User::patchToUrl($rank['picture_path']); ?>"/></td>
                                         <td><?php echo $rank['name']; ?></td>
                                         <td><?php echo $rank['rank']; ?></td>
