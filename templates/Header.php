022e9bb55b22b44e47855c0228bde858104425c2
Medhi Bechina <medhi.bechina@lazada.com>
2016-07-30 20:31:44 +0700

    Print user email properly

--- templates/Header.php
+++ templates/Header.php
@@ -3,11 +3,11 @@
   <div class="col-sm-6 col-lg-3">
     <div class="card-box widget-user">
       <div>
-        <img src="<?php echo $_SESSION['user_picture']; ?>" class="img-responsive img-circle" alt="user">
+        <img src="http://<?php echo $_SESSION['user_picture']; ?>" class="img-responsive img-circle" alt="user">
         <div class="wid-u-info">
           <h4 class="m-t-0 m-b-5"><?php echo $_SESSION['user_name']; ?></h4>
-          <p class="text-muted m-b-5 font-13"><?php echo '<pre>'; print_r($_SESSION); echo '</pre>'; ?></p>
-          <small class="text-warning"><b>Gamer - Rookie</b></small>
+          <p class="text-muted m-b-5 font-13"><?php echo $_SESSION['user_email']; ?></p>
+          <small class="text-success"><b>Rookie</b></small>
         </div>
       </div>
     </div>
