887afc780cdf5bae7cee637b76a548a718832103
vpoturaev <viacheslav.poturaev@lazada.com>
2016-10-21 16:06:46 +0700

    conf

--- env/conf.dist.php
+++ env/conf.dist.php
@@ -3,13 +3,36 @@
 namespace Rankster;
 
 use Rankster\Service\Facebook;
-use Rankster\Service\Facebook\Settings;
+use Rankster\Service\Google;
 use Yaoi\Database;
+use Yaoi\Log;
 
-Database::register('mysqli://root:password@localhost/rankster');
-Facebook::register(function(){
-    $settings = new Settings();
-    $settings->appId = 'app-id';
-    $settings->appSecret = 'app-secret';
+if (isset($_COOKIE['tz'])) {
+    date_default_timezone_set($_COOKIE['tz']);
+}
+
+Database::register(function () {
+    $config = new Database\Settings('mysqli://root@localhost/rankster2');
+    if (isset($_COOKIE['tz'])) {
+        $config->timezone = $_COOKIE['tz'];
+    }
+    $db = new Database($config);
+    $db->log(new Log('stdout'));
+    return $db;
+});
+
+Facebook::register(function () {
+    $settings = new Facebook\Settings();
+    $settings->appId = 'aaa';
+    $settings->appSecret = 'aaa';
+    return $settings;
+});
+
+
+Google::register(function () {
+    $settings = new Google\GoogleSettings();
+    $settings->clientId = 'aaa';
+    $settings->secret = 'aaa';
+    $settings->redirectUrl = 'http://' . $_SERVER['HTTP_HOST'] . '/v1/oauth/google';
     return $settings;
 });
